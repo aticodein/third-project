2be0aaa7050bfaea56b129c9c1fa150edc2c058a
Ubuntu <ubuntu@ip-172-31-37-110.ec2.internal>
2019-10-01 07:18:53 +0000

    connect to MongoDB with templates

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'myHunCuisine'
+app.config["MONGO_URI"] = 'mongodb+srv://ati_user:Zxcv4542@hungariancuisine-7dmod.mongodb.net/myHunCuisine?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
-
+@app.route('/myHunCuisineDB')
+def get_myHunCuisineDB():
+    return render_template("huncuisine.html", myHunCuisineDB=mongo.db.myHunCuisineDB.find())
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
