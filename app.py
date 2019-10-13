168e1ee0657f0705ce187e2d43e7ecf95bad5a3c
Ubuntu <ubuntu@ip-172-31-37-110.ec2.internal>
2019-10-13 10:26:17 +0000

    edit category

--- app.py
+++ app.py
@@ -58,6 +58,19 @@ def delete_recipe(recipe_id):
 def get_categories():
     return render_template('categories.html', categories=mongo.db.categories.find())
     
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one(
+                           {'_id': ObjectId(category_id)}))
+                           
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))                           
+    
     
 if __name__ == '__main__':
     
