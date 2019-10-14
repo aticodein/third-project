d08a539dd560cae22d57744e9b71bbe662bda80e
Ubuntu <ubuntu@ip-172-31-37-110.ec2.internal>
2019-10-14 14:51:11 +0000

    edit recipe fixing

--- app.py
+++ app.py
@@ -12,9 +12,14 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
+
 @app.route('/myHunCuisineDB')
 def get_myHunCuisineDB():
      return render_template("huncuisine.html", myHunCuisineDB=mongo.db.myHunCuisineDB.find(), page_title="Recipes")
+     
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', categories=mongo.db.categories.find())     
                           
 @app.route('/base')
 def get_base():
@@ -30,19 +35,26 @@ def insert_recipe():
     myHunCuisineDB.insert_one(request.form.to_dict())
     return redirect(url_for('get_myHunCuisineDB'))
     
-@app.route('/edit_recipe/<recipe_id>')
+@app.route('/edit_recipe/<recipe_id>', methods=['GET'])
 def edit_recipe(recipe_id):
     the_recipe = mongo.db.myHunCuisineDB.find_one({"_id":ObjectId(recipe_id)})
-    all_categories = mongo.db.categories.find()
-    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories, page_title="Edit Recipes")
+    return render_template('editrecipe.html', recipe=the_recipe, categories=get_categories, page_title="Edit Recipes")
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories')) 
     
-@app.route('/update_recipe/<recipe_id>', methods=["POST"])
-def update_recipe(recipe_id):
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id, category_id):
     recipe = mongo.db.myHunCuisineDB
+    category = mongo.db.categories
     recipe.update( {'_id': ObjectId(recipe_id)},
     {
+        'category_name':request.form.get('get_categories'),
         'recipe_name':request.form.get('recipe_title'),
-        'category_name':request.form.get('category_name'),
         'recipe_ingredients': request.form.get('recipe_ingredients'),
         'recipe_preparation': request.form.get('recipe_preparation'),
         'is_favourite':request.form.get('is_favourite')
@@ -54,22 +66,11 @@ def delete_recipe(recipe_id):
     mongo.db.myHunCuisineDB.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_myHunCuisineDB'))  
     
-@app.route('/get_categories')
-def get_categories():
-    return render_template('categories.html', categories=mongo.db.categories.find())
-    
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
                            category=mongo.db.categories.find_one(
                            {'_id': ObjectId(category_id)}))
-                           
-@app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id):
-    mongo.db.categories.update(
-        {'_id': ObjectId(category_id)},
-        {'category_name': request.form.get('category_name')})
-    return redirect(url_for('get_categories'))                           
     
     
 if __name__ == '__main__':
