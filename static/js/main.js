161b6483e457ffe16843e6fa50368c0af5a5bae5
Ubuntu <ubuntu@ip-172-31-37-110.ec2.internal>
2019-10-19 12:43:10 +0000

    working on favourite switch

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,19 @@ $(document).ready(function() {
      $('select').material_select();
      $(".collapsible").collapsible();
      $(".button-collapse").sideNav();
+     $(".tooltipped").tooltip();
 });    
+
+
+ function changeFavourite() {
+   const element = document.getElementById("favourite");
+   if (element.innerText == "ON") {
+       element.innerText = "Favourite";
+   }
+   else {
+       element.style.display = "none";
+   }
+} 
+
+changeFavourite();
+ 
