b6823500946619c3481e78b380fbaa362c1750c0
Ubuntu <ubuntu@ip-172-31-37-110.ec2.internal>
2019-10-27 09:21:32 +0000

    sticky navbar with js

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,4 @@
-alert("only main js loaded trogh base"); 
- 
- 
- // Materialize Framework JQuery Iniitialisations  
+// Materialize Framework JQuery Iniitialisations  
  
 $(document).ready(function() {
      $('select').material_select();
@@ -14,4 +11,17 @@ function checkDelete(){
    return confirm("Are you sure you want to delete this?");
 }   
             
- 
+const nav = document.querySelector('#sticky-nav');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = '100px';
+        document.body.classList.add("navbar-fixed");
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove("navbar-fixed");
+    }
+}
+
+window.addEventListener('scroll', fixNav);
